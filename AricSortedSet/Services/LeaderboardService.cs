3f18bf265f2d24c98c253b720aea4e57ea479096
zl-aric <17603075200@163.com>
2025-07-14 17:26:55 +0800

    更新为读写锁

--- AricSortedSet/Services/LeaderboardService.cs
+++ AricSortedSet/Services/LeaderboardService.cs
@@ -6,34 +6,37 @@ namespace AricTest.Services
     public class LeaderboardService
     {
         private readonly SortedSet<Customer> _leaderboard = new();
-        private readonly Dictionary<long, Customer> _customers = new();
-        private readonly object _lock = new();
+        private readonly ConcurrentDictionary<long, Customer> _customers = new();
+        private readonly ReaderWriterLockSlim _lock = new();
 
         public decimal UpdateScore(long customerId, decimal scoreChange)
         {
             if (scoreChange < -1000 || scoreChange > 1000)
                 throw new ArgumentOutOfRangeException(nameof(scoreChange), "Score must be between -1000 and 1000");
 
-            lock (_lock)
+            //只有大于》0的分数才会在排行榜上显示
+            if (_customers.TryGetValue(customerId, out var customer))
             {
-                //只有大于》0的分数才会在排行榜上显示
-                if (_customers.TryGetValue(customerId, out var customer))
-                {
-                    _leaderboard.Remove(customer);
-                    customer.Score += scoreChange;
-                    if (customer.Score > 0)
-                        _leaderboard.Add(customer);
-                }
-                else
-                {
-                    customer = new Customer { CustomerID = customerId, Score = scoreChange };
-                    _customers[customerId] = customer;
-                    if (customer.Score > 0)
-                        _leaderboard.Add(customer);
-                }
-
-                return customer.Score;
+                _lock.EnterWriteLock();
+                _leaderboard.Remove(customer);
+                customer.Score += scoreChange;
+                if (customer.Score > 0)
+                    _leaderboard.Add(customer);
+                _lock.ExitWriteLock();
             }
+            else
+            {
+                customer = new Customer { CustomerID = customerId, Score = scoreChange };
+                _customers[customerId] = customer;
+                if (customer.Score > 0)
+                {
+                    _lock.EnterWriteLock();
+                    _leaderboard.Add(customer);
+                    _lock.ExitWriteLock();
+                }
+            }
+
+            return customer.Score;
         }
 
         public List<CustomerDto> GetByRank(int startRank, int endRank)
@@ -41,7 +44,8 @@ namespace AricTest.Services
             if (startRank < 1 || endRank < startRank)
                 throw new ArgumentException("Invalid rank range");
 
-            lock (_lock)
+            _lock.EnterReadLock();
+            try
             {
                 var takeCount = endRank - startRank + 1;
                 var result = new List<CustomerDto>(takeCount);
@@ -59,17 +63,22 @@ namespace AricTest.Services
                 }
                 return result;
             }
+            finally
+            {
+                _lock.ExitReadLock();
+            }
         }
 
         public List<CustomerDto> GetCustomerWithNeighbors(long customerId, int high = 0, int low = 0)
         {
-            lock (_lock)
-            {
-                if (!_customers.TryGetValue(customerId, out var customer))
-                    throw new KeyNotFoundException("Customer not found");
-                if (customer.Score <= 0)
-                    return [];
+            if (!_customers.TryGetValue(customerId, out var customer))
+                throw new KeyNotFoundException("Customer not found");
+            if (customer.Score <= 0)
+                return [];
 
+            _lock.EnterReadLock();
+            try
+            {
                 var lowerView = _leaderboard.GetViewBetween(_leaderboard.Min, customer);
                 int index = lowerView.Count - 1;
 
@@ -95,8 +104,10 @@ namespace AricTest.Services
 
                 return result;
             }
+            finally
+            {
+                _lock.ExitReadLock();
+            }
         }
-
     }
 }
-
